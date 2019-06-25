fd7cba08abb520a5407466e8b3f3cb6336c7ad88
hseiler <38227827+hseiler@users.noreply.github.com>
2019-06-25 23:09:00 +0200

    Add tests folder

---
+++ tests/test_other_things.py
@@ -0,0 +1,9 @@
+def test_string():
+    a = 'some'
+    b = 'some'
+    assert a == b
+
+def test_boolean():
+    a = True
+    b = True
+    assert a == b
---
+++ tests/test_thing.py
@@ -0,0 +1,10 @@
+def test_string():
+    a = 'some'
+    b = 'some'
+    assert a == b
+
+def test_boolean():
+    a = True
+    b = True
+    assert a == b
+
